7883d3d101bd5c4995dc51d3ef946f3e502d5cdc
MrMasterplan <simon@heisterkamp.dk>
2011-04-18 13:26:00 +0200

    Formatting

--- full_cycle/sframe_create_full_cycle.py
+++ full_cycle/sframe_create_full_cycle.py
@@ -4,9 +4,9 @@
 #* @Project: SFrame - ROOT-based analysis framework for ATLAS
 #* @Package: Core
 #*
-#* @author Stefan Ask			 <Stefan.Ask@cern.ch>					 - Manchester
-#* @author David Berge			<David.Berge@cern.ch>					- CERN
-#* @author Johannes Haller	<Johannes.Haller@cern.ch>			- Hamburg
+#* @author Stefan Ask             <Stefan.Ask@cern.ch>                     - Manchester
+#* @author David Berge            <David.Berge@cern.ch>                    - CERN
+#* @author Johannes Haller    <Johannes.Haller@cern.ch>            - Hamburg
 #* @author A. Krasznahorkay <Attila.Krasznahorkay@cern.ch> - CERN/Debrecen
 #*
 #***************************************************************************
@@ -31,53 +31,47 @@ import optparse
 # from outside using the usual method. (Which I've seen in various
 # places...)
 def main():
-	# Print some welcome message before doing anything else:
-	print ">>"
-	print ">> %s : Analysis cycle torso creator" % \
-				os.path.basename( sys.argv[ 0 ] )
-	print ">>"
-	print ""
-
-	# Parse the command line parameters:
-	parser = optparse.OptionParser( usage="%prog [options]" )
-	parser.add_option( "-n", "--name", dest="cycleName", action="store",
-						type="string", default="AnalysisCycle",
-						help="Name of the analysis cycle to create" )
-	#
-	parser.add_option( "-l", "--linkdef", dest="linkdef", action="store",
-						type="string", default="",
-						help="Name of the LinkDef.h file in the package" )
-	#
-	parser.add_option( "-r", "--rootfile", dest="rootfile", action="store",
-						type="string", default="",
-						help="Name a rootfile to use as the input. The tree name and the variable list can be read from here." )
-	#
-	parser.add_option( "-t", "--treename", dest="treename", action="store",
-						type="string", default="",
-						help="Name of the tree in the input file" )
-	#
-	parser.add_option( "-v", "--varlist", dest="varlist", action="store",
-						type="string", default="",
-						help="Name a file containing a list of variable declarations to be used in the cycle. Can contain comments." )
-	#
-	parser.add_option( "--analysis", dest="analysis", action="store",
-						type="string", default="",
-						help="Name of the analysis package. Defaults to the name of the pwd." )
-	
-	parser.add_option( "--outtree", dest="outtree", action="store",
-						type="string", default="",
-						help="Name of the output tree, if it should be written." )
-	
-	( options, args ) = parser.parse_args()
-
-	# This is where the main function are:
-	from FullCycleCreators import CycleCreator
-
-	# Execute the cycle creation:
-	cc = CycleCreator()
-	cc.CreateCycle( **options.__dict__)
+    # Print some welcome message before doing anything else:
+    print ">>"
+    print ">> %s : Analysis cycle torso creator" % \
+                os.path.basename( sys.argv[ 0 ] )
+    print ">>"
+    print ""
+    
+    # Parse the command line parameters:
+    parser = optparse.OptionParser( usage="%prog [options]" )
+    parser.add_option( "-n", "--name", dest="cycleName", action="store",
+                        type="string", default="AnalysisCycle",
+                        help="Name of the analysis cycle to create" )
+    parser.add_option( "-l", "--linkdef", dest="linkdef", action="store",
+                        type="string", default="",
+                        help="Name of the LinkDef.h file in the package" )
+    parser.add_option( "-r", "--rootfile", dest="rootfile", action="store",
+                        type="string", default="",
+                        help="Name a rootfile to use as the input. The tree name and the variable list can be read from here." )
+    parser.add_option( "-t", "--treename", dest="treename", action="store",
+                        type="string", default="",
+                        help="Name of the tree in the input file" )
+    parser.add_option( "-v", "--varlist", dest="varlist", action="store",
+                        type="string", default="",
+                        help="Name a file containing a list of variable declarations to be used in the cycle. Can contain comments." )
+    parser.add_option( "--analysis", dest="analysis", action="store",
+                        type="string", default="",
+                        help="Name of the analysis package. Defaults to the name of the pwd." )
+    parser.add_option( "--outtree", dest="outtree", action="store",
+                        type="string", default="",
+                        help="Name of the output tree, if it should be written." )
+    
+    ( options, args ) = parser.parse_args()
+    
+    # This is where the main function are:
+    from FullCycleCreators import CycleCreator
+    
+    # Execute the cycle creation:
+    cc = CycleCreator()
+    cc.CreateCycle( **options.__dict__)
 
 
 # Call the main function:
 if __name__ == "__main__":
-	main()
+    main()
