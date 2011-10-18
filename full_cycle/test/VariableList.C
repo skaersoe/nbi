b9ea48007d6b3b6a08ca082c62941e8a5ae5c0bd
MrMasterplan <simon@heisterkamp.dk>
2011-10-18 15:39:47 +0200

    Removed the code. I don't want people to use this anymore

--- full_cycle/test/VariableList.C
+++
@@ -1,8 +0,0 @@
-// This is a comment
-// comments are excluded as long as they don't look like declarations.
-//UInt_t RunNumber;
-int i;
-//float f;
-vector<float> *v;/*
-vector<float> *el_eta;
-vector<float> *el_phi;*/
